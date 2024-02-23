31b77d87cb5b839d0f8d953358dd44291b45d75f
Zuhair <zuhair.gulzar@originbluy.com>
2024-02-23 08:09:39 -0500

    Hydartion Error fixed

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,5 +1,5 @@
 import Recommended, { RecommendedSkeleton } from "./recommended";
-import Toggle from "./toggle";
+import Toggle, { ToggleSkeleton } from "./toggle";
 
 import { getRecommended } from "@/lib/recommended-service";
 
@@ -20,6 +20,7 @@ const Sidebar = async () => {
 export const SidebarSkeleton = () => {
   return (
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2d2e35] z-50">
+      <ToggleSkeleton />
       <RecommendedSkeleton />
     </aside>
   );
--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -4,6 +4,7 @@ import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
 import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/store/user-sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Toggle = () => {
   const { collapsed, onCollapse, onExpand } = useSidebar((state) => state);
@@ -39,5 +40,14 @@ const Toggle = () => {
     </>
   );
 };
+
+export const ToggleSkeleton = () => {
+  return (
+    <div className="p-3 pl-6 mb-2 hidden lg:flex items-center justify-between w-full">
+      <Skeleton className="h-6 w-[100px]" />
+      <Skeleton className="h-6 w-6" />
+    </div>
+  )
+}
 
 export default Toggle;
